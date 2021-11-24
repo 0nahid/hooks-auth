ae7701a443309bcf2691bfa49b73f1bac07f5c7b
Nahid Hassan Bulbul <nahidhassanbulbul@gmail.com>
2021-11-24 18:31:57 +0600

    reset margin padding , added custom font

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./Header.css";
 
 export default function Header() {
   return (
-    <div>
+    <div className="header">
       <Link to="/">Home</Link>
       <Link to="/login">Login</Link>
       <Link to="/register">Register</Link>
